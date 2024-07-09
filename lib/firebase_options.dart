8c7a1b111eb8d7df36363fe5a9643f9116551f66
Dave Gomez <davegomez426@gmail.com>
2024-07-09 16:01:55 -0400

    FCM integrado y funcional

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -65,4 +65,5 @@ class DefaultFirebaseOptions {
     storageBucket: 'flutter-projects-f3ceb.appspot.com',
     iosBundleId: 'com.davegomez.pushApp',
   );
+
 }
--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,9 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -9,9 +12,51 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState()) {
-    on<NotificationsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    _initialStatusCheck();
+    _onForegroundMessage();
+  }
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+
+    FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  }
+
+  void _notificationStatusChanged(
+      NotificationStatusChanged event, Emitter<NotificationsState> emit) {
+    emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+    _getFCMToken();
+  }
+
+  void _getFCMToken() async {
+    final settings = await messaging.getNotificationSettings();
+    if (state.status != AuthorizationStatus.authorized) return;
+
+    final token = await messaging.getToken();
+    print(token);
+  }
+
+  void _handelRemoteMessage(RemoteMessage message) {
+    print('Encntre un nuevo mensaje');
+
+    print('msessage data ${message.data}');
+
+    if (message.notification != null) return;
+
+    print('message also contiene ${message.notification}');
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handelRemoteMessage);
   }
 
   void requestPersimssion() async {
@@ -23,5 +68,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
         criticalAlert: true,
         provisional: false,
         sound: true);
+
+    add(NotificationStatusChanged(settings.authorizationStatus));
+    _getFCMToken();
   }
 }
